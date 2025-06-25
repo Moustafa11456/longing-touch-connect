d6f9cc02ee288e8a34557fb2463a99c24515435f
Moustafa11456 <safo12345safoo@gmail.com>
2025-06-26 00:15:33 +0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://tnsvztqgtukktjrizrpq.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuc3Z6dHFndHVra3Rqcml6cnBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2OTI0OTAsImV4cCI6MjA2NjI2ODQ5MH0.VMmbk0hSENA6fl8d1m5NlV2moDTr8bDe_XPQhj0-dAs';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuc3Z6dHFndHVra3Zqcml6cnBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2OTI0OTAsImV4cCI6MjA2NjI2ODQ5MH0.VMmbk0hSENA6fl8d1m5NlV2moDTr8bDe_XPQhj0-dAs';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
